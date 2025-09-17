d0ec6264546d78b648b5c9a7a2612266dbec102a
yuhsuan-t <12108766+yuhsuan-t@users.noreply.github.com>
2025-09-16 17:51:49 -0700

    Update Hillside 48 keymap

--- keyboards/hillside/48/keymaps/yuhsuan-t/keymap.c
+++ keyboards/hillside/48/keymaps/yuhsuan-t/keymap.c
@@ -14,13 +14,9 @@ enum layer {
 };
 
 // Layer toggle
-#define UDS_NAV LT(_NAV, KC_UNDS)
 #define SPC_NAV LT(_NAV, KC_SPC)
-#define MIN_UTL LT(_UTIL, MINUS)
+#define ESC_NAV LT(_NAV, KC_ESC)
 #define MO_UTIL MO(_UTIL)
-#define EQL_NUM LT(_NUMPAD, KC_EQL)
-#define EN_NUM LT(_NUMPAD, KC_ENT)
-#define ESC_NUM LT(_NUMPAD, KC_ESC)
 #define MO_NAV MO(_NAV)
 #define MO_NUM MO(_NUMPAD)
 
@@ -68,36 +64,35 @@ enum layer {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_HANDS_DOWN] = LAYOUT(
                  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,
-                 KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SLSH, KC_COLN,
-                 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT, LOGOUT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN, KC_PIPE,
-                                   KC_LGUI, ESC_NUM, MO_UTIL, SPC_NAV, KC_ENT,  MINUS,  MO_NAV,  KC_UNDS, XXXXXXX, XXXXXXX
+                 KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_PSLS, KC_COLN,
+                 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT, LOGOUT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  XXXXXXX, KC_PIPE,
+                                   KC_LGUI, ESC_NAV, MO_UTIL, MO_NUM,  KC_ENT,  MO_NAV,  KC_SPC,  KC_UNDS, MINUS,   XXXXXXX
     ),
     [_NAV] = LAYOUT(
                  _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX,                   XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_PGUP,
                  _______, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,                    KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  KC_PGDN,
                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+                                   _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_UTIL] = LAYOUT(
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SAVE,                      SAVE,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                  _______, XXXXXXX, BY_NWIN, CUT,     COPY,    PASTE,                     PASTE,   COPY,    CUT,     BY_NWIN, XXXXXXX, XXXXXXX,
                  _______, XXXXXXX, BY_RNWN, BY_FPNE, BY_CLYT, BY_HSPL, _______, _______, BY_HSPL, BY_CLYT, BY_FPNE, BY_RNWN, XXXXXXX, XXXXXXX,
-                                   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+                                   _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_NUMPAD] = LAYOUT(
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_EQL,  KC_P7,   KC_P8,   KC_P9,   KC_ASTR, KC_PLUS,
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PDOT, KC_P4,   KC_P5,   KC_P6,   KC_PSLS, KC_COLN,
                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX,
-                                   _______, _______, _______, _______, _______, KC_PMNS, _______, KC_UNDS, XXXXXXX, XXXXXXX
+                                   _______, _______, _______, _______, _______, _______, KC_SPC,  KC_UNDS, KC_PMNS, XXXXXXX
     ),    
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_HANDS_DOWN] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_NAV]        = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_UTIL]       = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_NUMPAD]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_FFXIV]      = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_HANDS_DOWN] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
+    [_NAV]        = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
+    [_UTIL]       = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
+    [_NUMPAD]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
 };
 #endif
--- users/yuhsuan-t/combos.h
+++ users/yuhsuan-t/combos.h
@@ -6,15 +6,17 @@ const uint16_t PROGMEM ytseng[] = {KC_Y, KC_T, COMBO_END};
 const uint16_t PROGMEM yuhsuan[] = {KC_Y, KC_H, COMBO_END};
 const uint16_t PROGMEM thanks[] = {KC_T, KC_K, COMBO_END};
 const uint16_t PROGMEM trtllm[] = {KC_T, KC_L, COMBO_END};
-const uint16_t PROGMEM sglang[] = {KC_S, KC_L, COMBO_END};
+const uint16_t PROGMEM sglang[] = {KC_S, KC_G, COMBO_END};
 const uint16_t PROGMEM lbrc[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM rbrc[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM lquot[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM rquot[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM lprn[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM rprn[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM lt[] = {KC_P1, KC_P2, COMBO_END};
+const uint16_t PROGMEM gt[] = {KC_P2, KC_P3, COMBO_END};
 const uint16_t PROGMEM exclam[] = {KC_X, KC_C, COMBO_END};
-const uint16_t PROGMEM at[] = {KC_A, KC_T, COMBO_END};
+const uint16_t PROGMEM at[] = {KC_Q, KC_T, COMBO_END};
 const uint16_t PROGMEM hash[] = {KC_H, KC_S, COMBO_END};
 const uint16_t PROGMEM dollar[] = {KC_D, KC_L, COMBO_END};
 const uint16_t PROGMEM percent[] = {KC_P, KC_C, COMBO_END};
@@ -23,7 +25,9 @@ const uint16_t PROGMEM ampersand[] = {KC_A, KC_M, COMBO_END};
 const uint16_t PROGMEM tilde[] = {KC_T, KC_D, COMBO_END};
 const uint16_t PROGMEM backslash[] = {KC_B, KC_S, COMBO_END};
 const uint16_t PROGMEM plus[] = {KC_P, KC_L, COMBO_END};
+const uint16_t PROGMEM semicolon[] = {KC_S, KC_C, COMBO_END};
 const uint16_t PROGMEM id[] = {KC_I, KC_D, COMBO_END};
+const uint16_t PROGMEM question[] = {KC_Q, KC_W, COMBO_END};
 
 enum combo_events {
     COMBO_YTSENG,
@@ -37,6 +41,8 @@ enum combo_events {
     COMBO_RQUOT,
     COMBO_LPRN,
     COMBO_RPRN,
+    COMBO_LT,
+    COMBO_GT,
     COMBO_EXCLAM,
     COMBO_AT,
     COMBO_HASH,
@@ -47,7 +53,9 @@ enum combo_events {
     COMBO_TILDE,
     COMBO_BACKSLASH,
     COMBO_PLUS,
+    COMBO_SEMICOLON,
     COMBO_ID,
+    COMBO_QUESTION,
 };
 
 combo_t key_combos[] = {
@@ -62,6 +70,8 @@ combo_t key_combos[] = {
     [COMBO_RQUOT] = COMBO(rquot, KC_QUOT),
     [COMBO_LPRN] = COMBO(lprn, LPRN),
     [COMBO_RPRN] = COMBO(rprn, RPRN),
+    [COMBO_LT] = COMBO(lt, KC_LT),
+    [COMBO_GT] = COMBO(gt, KC_GT),
     [COMBO_EXCLAM] = COMBO(exclam, KC_EXLM),
     [COMBO_AT] = COMBO(at, KC_AT),
     [COMBO_HASH] = COMBO(hash, KC_HASH),
@@ -72,5 +82,7 @@ combo_t key_combos[] = {
     [COMBO_TILDE] = COMBO(tilde, KC_TILD),
     [COMBO_BACKSLASH] = COMBO(backslash, KC_BSLS),
     [COMBO_PLUS] = COMBO(plus, KC_PLUS),
+    [COMBO_SEMICOLON] = COMBO(semicolon, KC_SCLN),
     [COMBO_ID] = COMBO(id, ID),
+    [COMBO_QUESTION] = COMBO(question, KC_QUES),
 };
